15413cfc1542a2904e1dd5f7a5052c55da6cbf31
Lucas <bithome1@outlook.com>
2022-08-10 17:26:52 -0300

    tentando corrigir bug de upsidedown

--- index.js
+++ index.js
@@ -18,16 +18,21 @@ app.use(cors());
 
 /* source: https://www.samanthaming.com/tidbits/19-2-ways-to-convert-to-boolean/ */
 const vlBolean = process.env.UPSIDEDOWN_MODE;
-const hereIsTheUpsideDown = (vlBolean === 'true') ? !!vlBolean : !vlBolean;
+const hereIsTheUpsideDown = () => {
+  if (vlBolean === 'true') {
+    return true;
+  }
+  return false;
+};
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    hereIsTheUpsideDown(),
   );
 
   res.status(200).json(characters);
-});//
+});
 
 app.listen(/* 3000 */ port, () => {
   console.log('Escutando na porta 3000');
